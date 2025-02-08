e7da1a4a4415a112715a9f378caf5208ba062b64
Gosetti Sowmya <gosettisowmya@gmail.com>
2025-02-08 09:52:38 +0530

    message

---
+++ largestnumber.py
@@ -0,0 +1,13 @@
+a=int(input("enter the first value a:"))
+b=int(input("enter the second value b:"))
+c=int(input("enter the third value c:"))
+if(a>b):
+    if(a>c):
+        print(a,"is the largest number")
+    else:
+        print(c,"is the largest number")
+else:
+    if(b>c):
+        print(b,"is the largest number")
+    else:
+        print (c,"is the largest number")          
